314d1f7f233ba55a8320a22b2c911d9055ace395
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2024-04-16 14:58:26 -0300

    Separating date utils to own file Handle all UI changes in functions to have a clean initialization and event handling

---
+++ date-utils.js
@@ -0,0 +1,150 @@
+/**
+ * Returns an array of Date objects representing the days of the specified month and year.
+ *
+ * @param {number} year - The year for which the days of the month are to be retrieved.
+ * @param {number} month - The month for which the days are to be retrieved.
+ * @return {Array<Date>} An array of Date objects representing the days of the month.
+ */
+export function getDaysOfMonth(year, month) {
+  const firstDayOfMonth = new Date(year, month, 1);
+  const days = [];
+  while (firstDayOfMonth.getMonth() === month) {
+    days.push(new Date(firstDayOfMonth));
+    firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+  }
+  return days;
+}
+
+/**
+ * Returns an array of arrays, each representing a week of the specified month and year.
+ *
+ * @param {number} year - The year for which the weeks are to be retrieved.
+ * @param {number} month - The month for which the weeks are to be retrieved.
+ * @return {Array<Array<Date>>} An array of arrays, each containing Date objects representing the days of a week.
+ */
+export function getWeeksOfMonth(year, month) {
+  const days = getDaysOfMonth(year, month);
+  const weeks = [];
+  let currentWeek = [];
+
+  for (const day of days) {
+    if (currentWeek.length === 0 || day.getDay() === 0) {
+      if (currentWeek.length > 0) {
+        weeks.push(currentWeek);
+      }
+      currentWeek = [day];
+    } else {
+      currentWeek.push(day);
+    }
+  }
+
+  if (currentWeek.length > 0) {
+    weeks.push(currentWeek);
+  }
+
+  AdjustFirstWeek();
+
+  adjustLastWeek();
+
+  return weeks;
+
+  /**
+   * A function that adjusts the first week of a month if needed.
+   *
+   * @return {void} This function does not return anything.
+   */
+  function AdjustFirstWeek() {
+    const firstDay = days[0].getDay();
+    if (firstDay === 0) {
+      return;
+    }
+
+    const firstWeek = weeks[0];
+    const prevMonth = new Date(year, month - 1, 1);
+    const prevMonthDays = getDaysOfMonth(
+      prevMonth.getFullYear(),
+      prevMonth.getMonth()
+    ).length;
+    const daysToAdd = 7 - firstWeek.length;
+    let missingDay = 0;
+    for (let i = prevMonthDays - daysToAdd; i < prevMonthDays; i++) {
+      firstWeek.splice(
+        missingDay++,
+        0,
+        new Date(prevMonth.getFullYear(), prevMonth.getMonth(), i + 1)
+      );
+    }
+  }
+
+/**
+ * Adjusts the last week of a month if needed.
+ *
+ * @return {void} This function does not return anything.
+ */
+  function adjustLastWeek() {
+    const lastDay = days[days.length - 1].getDay();
+    const lastWeek = weeks[weeks.length - 1];
+    if (lastDay === 6 && lastWeek.length === 7) {
+      return;
+    }
+    const nextMonth = new Date(year, month + 1, 1);
+    const daysToAdd = 6 - lastDay;
+    for (let i = 1; i <= daysToAdd; i++) {
+      lastWeek.push(new Date(nextMonth.getFullYear(), nextMonth.getMonth(), i));
+    }
+  }
+}
+
+/**
+ * Returns a string representing the month and year in a specified locale.
+ *
+ * @param {number} year - The year for which the month and year are to be formatted.
+ * @param {number} month - The month for which the month and year are to be formatted.
+ * @return {string} A formatted string representing the month and year.
+ */
+export function getMonthAndYearWithLocale(year, month) {
+  return `${new Date(year, month).toLocaleString("default", {
+    month: "long",
+  })} ${year}`;
+}
+
+/**
+ * Returns an array of month names in the specified locale.
+ *
+ * @return {Array<string>} An array of month names.
+ */
+export function getMonthsWithLocale() {
+  const months = [];
+  const date = new Date();
+
+  for (let i = 0; i < 12; i++) {
+    date.setMonth(i);
+    months.push(date.toLocaleString("default", { month: "long" }));
+  }
+
+  return months;
+}
+
+/**
+ * Returns an array of localized day names for each day of the week.
+ *
+ * @return {Array<string>} An array of localized day names.
+ */
+export function getDaysOfWeekWithLocale() {
+  const days = [];
+  const date = new Date();
+
+  for (let i = 0; i < 7; i++) {
+    date.setDate(date.getDate() + (i + 1));
+    days.push({
+      index: date.getDay(),
+      name: date.toLocaleString("default", { weekday: "long" }),
+    });
+  }
+
+  days.sort((a, b) => a.index - b.index);
+
+  const sortedDays = days.map((day) => day.name);
+
+  return sortedDays;
+}
--- script.js
+++ script.js
@@ -1,169 +1,193 @@
+import {
+  getDaysOfWeekWithLocale,
+  getMonthAndYearWithLocale,
+  getMonthsWithLocale,
+  getWeeksOfMonth,
+} from "./date-utils.js";
+
 //#region Utils
-function getDaysOfMonth(year, month) {
-  const firstDayOfMonth = new Date(year, month, 1);
-  const days = [];
-  while (firstDayOfMonth.getMonth() === month) {
-    days.push(new Date(firstDayOfMonth));
-    firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
-  }
-  return days;
+
+/**
+ * Updates the calendar display based on the provided year and month.
+ *
+ * @param {number} year - The year for the calendar display.
+ * @param {number} month - The month for the calendar display.
+ */
+function updateCalendarDisplay(year, month) {
+  updateYearMonthElem(getMonthAndYearWithLocale(year, month));
+  updateCalendarElem(generateCalendarRowsAndColumns(year, month));
 }
 
-function getWeeksOfMonth(year, month) {
-  const days = getDaysOfMonth(year, month);
-  const weeks = [];
-  let currentWeek = [];
+/**
+ * Initializes the calendar with default year and month values, updates the calendar display, and initializes various elements.
+ *
+ * @return {{yearElem: HTMLInputElement, monthElem: HTMLSelectElement}} An object containing the initialized year and month elements.
+ */
+function initCalendar() {
+  const year = 2024;
+  const month = 3;
+  const yearElem = initYearElement(year);
+  const monthElem = initMonthElement(month);
 
-  for (const day of days) {
-    if (currentWeek.length === 0 || day.getDay() === 0) {
-      if (currentWeek.length > 0) {
-        weeks.push(currentWeek);
-      }
-      currentWeek = [day];
-    } else {
-      currentWeek.push(day);
-    }
-  }
+  updateCalendarDisplay(year, month);
+  setDateElement(getMonthAndYearWithLocale(year, month));
+  initDaysOfWeekElement();
 
-  if (currentWeek.length > 0) {
-    weeks.push(currentWeek);
-  }
-
-  AdjustFirstWeek();
-
-  adjustLastWeek();
-
-  return weeks;
-
-  function AdjustFirstWeek() {
-    const firstDay = days[0].getDay();
-    if (firstDay === 0) {
-      return;
-    }
-
-    const firstWeek = weeks[0];
-    const prevMonth = new Date(year, month - 1, 1);
-    const prevMonthDays = getDaysOfMonth(
-      prevMonth.getFullYear(),
-      prevMonth.getMonth()
-    ).length;
-    const daysToAdd = 7 - firstWeek.length;
-    let missingDay = 0;
-    for (let i = prevMonthDays - daysToAdd; i < prevMonthDays; i++) {
-      firstWeek.splice(
-        missingDay++,
-        0,
-        new Date(prevMonth.getFullYear(), prevMonth.getMonth(), i + 1)
-      );
-    }
-  }
-
-  function adjustLastWeek() {
-    const lastDay = days[days.length - 1].getDay();
-    const lastWeek = weeks[weeks.length - 1];
-    if (lastDay === 6 && lastWeek.length === 7) {
-      return;
-    }
-    const nextMonth = new Date(year, month + 1, 1);
-    const daysToAdd = 6 - lastDay;
-    for (let i = 1; i <= daysToAdd; i++) {
-      lastWeek.push(new Date(nextMonth.getFullYear(), nextMonth.getMonth(), i));
-    }
-  }
+  return { yearElem, monthElem };
 }
 
-function displayCalendar() {
-  const weeks = getWeeksOfMonth(year, month);
-  const calendarElem = document.getElementById("calendar");
-  const yearAndMonthElem = document.getElementById("yearAndMonth");
-
+/**
+ * Updates the calendar element with the provided weeks elements.
+ *
+ * @param {HTMLTableRowElement[]} weeksElem - An array of table row elements representing the weeks of the calendar.
+ * @param {string} [calendarId="calendar"] - The ID of the calendar element to update. Defaults to "calendar".
+ * @return {void} This function does not return anything.
+ */
+function updateCalendarElem(weeksElem, calendarId = "calendar") {
+  const calendarElem = document.getElementById(calendarId);
   calendarElem.innerHTML = "";
-  yearAndMonthElem.textContent = getMonthAndYearWithLocale();
+  weeksElem.forEach((weekElem) => calendarElem.appendChild(weekElem));
+}
 
-  for (let week of weeks) {
+/**
+ * Updates the year and month element with the provided content.
+ *
+ * @param {string} content - The content to be displayed in the year and month element.
+ * @param {string} [yearAndMonthId="yearAndMonth"] - The ID of the year and month element. Defaults to "yearAndMonth".
+ * @return {void} This function does not return anything.
+ */
+function updateYearMonthElem(content, yearAndMonthId = "yearAndMonth") {
+  const yearAndMonthElem = document.getElementById(yearAndMonthId);
+  yearAndMonthElem.textContent = content;
+}
+
+/**
+ * Generates the rows and columns of a calendar for the given year and month.
+ *
+ * @param {number} year - The year of the calendar.
+ * @param {number} month - The month of the calendar.
+ * @return {HTMLTableRowElement[]} An array of table row elements representing the weeks of the calendar.
+ */
+function generateCalendarRowsAndColumns(year, month) {
+  const weeks = getWeeksOfMonth(year, month);
+
+  /**
+   * @type {HTMLTableRowElement[]}
+   */
+  const weeksElem = [];
+  weeks.forEach((week) => {
     const weekElem = document.createElement("tr");
-    for (let day of week) {
+    week.forEach((day) => {
       const dayElem = document.createElement("td");
       dayElem.textContent = day.getDate();
       weekElem.appendChild(dayElem);
-    }
-    calendarElem.appendChild(weekElem);
-  }
-}
-
-function getMonthAndYearWithLocale() {
-  return `${new Date(year, month).toLocaleString("default", {
-    month: "long",
-  })} ${year}`;
-}
-
-function getMonthsWithLocale() {
-  const months = [];
-  const date = new Date();
-
-  for (let i = 0; i < 12; i++) {
-    date.setMonth(i);
-    months.push(date.toLocaleString("default", { month: "long" }));
-  }
-
-  return months;
-}
-
-function getDaysOfWeekWithLocale() {
-  const days = [];
-  const date = new Date();
-
-  for (let i = 0; i < 7; i++) {
-    date.setDate(date.getDate() + (i + 1));
-    days.push({
-      index: date.getDay(),
-      name: date.toLocaleString("default", { weekday: "long" }),
     });
+    weeksElem.push(weekElem);
+  });
+  return weeksElem;
+}
+
+/**
+ * Initializes the month element with the provided month and month element ID.
+ *
+ * @param {number} month - The month to be initialized.
+ * @param {string} [monthElemId="month"] - The ID of the month element. Defaults to "month".
+ * @return {HTMLElement} - The initialized month element.
+ */
+function initMonthElement(month, monthElemId = "month") {
+  const monthElem = document.getElementById(monthElemId);
+
+  if (!monthElem) {
+    throw Error(`Element with id ${monthElemId} not found`);
   }
 
-  days.sort((a, b) => a.index - b.index);
+  getMonthsWithLocale().forEach((month, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.textContent = month;
+    monthElem.appendChild(option);
+  });
 
-  const sortedDays = days.map((day) => day.name);
+  monthElem.value = month;
 
-  return sortedDays;
+  return monthElem;
+}
+
+/**
+ * Initializes the year element with the provided year and element ID, setting the value of the element to the given year.
+ *
+ * @param {number} year - The year to be initialized.
+ * @param {string} [yearElemId="year"] - The ID of the year element. Defaults to "year".
+ * @return {HTMLElement} The initialized year element.
+ */
+function initYearElement(year, yearElemId = "year") {
+  const yearElem = document.getElementById(yearElemId);
+
+  if (!yearElem) {
+    throw Error(`Element with id ${yearElemId} not found`);
+  }
+
+  yearElem.value = year;
+
+  return yearElem;
+}
+
+/**
+ * Initializes the days of the week element with the provided element ID.
+ *
+ * @param {string} [daysOfWeekElemId="daysOfWeek"] - The ID of the days of the week element. Defaults to "daysOfWeek".
+ * @return {void} This function does not return anything.
+ */
+function initDaysOfWeekElement(daysOfWeekElemId = "daysOfWeek") {
+  const daysOfWeekElem = document.getElementById(daysOfWeekElemId);
+
+  getDaysOfWeekWithLocale().forEach((day) => {
+    const header = document.createElement("th");
+    header.innerHTML = day;
+    daysOfWeekElem.appendChild(header);
+  });
+}
+
+/**
+ * Sets the value of the specified date element.
+ *
+ * @param {string} date - The date value to set.
+ * @param {string} [dateElemId="date"] - The ID of the date element. Defaults to "date".
+ * @return {void} This function does not return anything.
+ */
+function setDateElement(date, dateElemId = "date") {
+  const dateElem = document.getElementById(dateElemId);
+  dateElem.value = date;
+}
+
+/**
+ * Handles the change event of the calendar.
+ *
+ * @param {HTMLElement} yearElem - The year input element of the calendar.
+ * @param {HTMLElement} monthElem - The month select element of the calendar.
+ * @return {void} This function does not return anything.
+ */
+function handleCalendarChange(yearElem, monthElem) {
+  const year = parseInt(yearElem.value);
+  const month = parseInt(monthElem.value);
+  updateCalendarDisplay(year, month);
+  setDateElement(getMonthAndYearWithLocale(year, month));
 }
 //#endregion
 
 //#region Main
-const yearElem = document.getElementById("year");
-const monthElem = document.getElementById("month");
-const daysOfWeekElem = document.getElementById("daysOfWeek");
-const dateElem = document.getElementById("date");
+/**
+ * Initializes event listeners for the calendar.
+ */
+const { yearElem, monthElem } = initCalendar();
 
-let year = 2024;
-let month = 3;
+// Add event listeners to year and month elements for "change" event
+// to update calendar display and set the value of the date element
+yearElem.addEventListener("change", () =>
+  handleCalendarChange(yearElem, monthElem)
+);
+monthElem.addEventListener("change", () =>
+  handleCalendarChange(yearElem, monthElem)
+);
 
-getMonthsWithLocale().forEach((month, index) => {
-  const option = document.createElement("option");
-  option.value = index;
-  option.textContent = month;
-  monthElem.appendChild(option);
-});
-getDaysOfWeekWithLocale().forEach((day) => {
-  const header = document.createElement("th");
-  header.innerHTML = day;
-  daysOfWeekElem.appendChild(header);
-});
-
-yearElem.value = year;
-monthElem.value = month;
-dateElem.value = getMonthAndYearWithLocale();
-
-yearElem.addEventListener("change", () => {
-  year = parseInt(yearElem.value);
-  displayCalendar();
-  dateElem.value = getMonthAndYearWithLocale();
-});
-monthElem.addEventListener("change", () => {
-  month = parseInt(monthElem.value);
-  displayCalendar();
-  dateElem.value = getMonthAndYearWithLocale();
-});
-
-displayCalendar();
 //#endregion
